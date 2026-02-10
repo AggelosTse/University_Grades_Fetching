66ee9eadd52691bb49b8f90d18a0189f627d223f
AggelosTse <aggu8914@gmail.com>
2026-02-10 18:24:26 +0200

    changed the logic

--- public/script.js
+++ public/script.js
@@ -1,56 +1,40 @@
-const toggleVis = document.getElementById("togglevisibility");
-const passwordField = document.querySelector("#pass");
-
-toggleVis.addEventListener("click", function () {
-  const isHidden = passwordField.getAttribute("type") === "password";
-
-  passwordField.setAttribute("type", isHidden ? "text" : "password");
-
-  if (isHidden) {
-    this.classList.remove("fa-eye-slash");
-    this.classList.add("fa-eye");
-  } else {
-    this.classList.remove("fa-eye");
-    this.classList.add("fa-eye-slash");
-  }
-});
-
-async function call() {
+async function call(boolAutomation) {
   const UImessage = document.getElementById("demo");
   UImessage.style.display = "block";
   UImessage.innerHTML = "Processing...";
   UImessage.className = "show";
 
-  const nameInput = document.getElementById("name").value;
-  const passInput = document.getElementById("pass").value;
-  const emailInput = document.getElementById("email").value;
+  if (boolAutomation === 'yes') {
+    const response = await fetch("/startAutomation", {
+      //automation trigger request
+      method: "POST",
+    });
 
-  document.getElementById("name").value = "";
-  document.getElementById("pass").value = "";
-  document.getElementById("email").value = "";
+    const data = await response.json();
 
-  const userInfo = {
-    userName: nameInput,
-    userPassword: passInput,
-    userEmail: emailInput,
-  };
+    if (data.type === "Failure") {
+      UImessage.className = "show error";
+      UImessage.innerHTML = data.message;
+      return;
+    }
 
-  const response = await fetch("/fetch-grades", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(userInfo),
-  });
-
-  const data = await response.json();
-
-  if (data.type === "Failure") {
-    UImessage.className = "show error";
+    UImessage.className = "show success";
     UImessage.innerHTML = data.message;
-    return;
-  }
+  } 
+  else {
+    const response = await fetch("/stopAutomation", {
+      //automation stop trigger request
+      method: "POST",
+    });
 
-  UImessage.className = "show success";
-  UImessage.innerHTML = data.message;
+    const data = await response.json();
+
+    if (data.type === "Failure") {
+      UImessage.className = "show error";
+      UImessage.innerHTML = data.message;
+      return;
+    }
+    UImessage.className = "show success";
+    UImessage.innerHTML = data.message;
+  }
 }
--- src/server/server.js
+++ src/server/server.js
@@ -2,169 +2,87 @@ import express from "express";
 import getData from "../services/dataFetcher.js";
 import getFreshCookie from "../services/getCookie.js";
 
-import cron from 'node-cron';
+import cron from "node-cron";
 
 import { fileURLToPath } from "url";
 import path from "path";
 
 import dotenv from "dotenv";
-import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, 
-  max: 10, 
-  message: "Too many requests, please try again later."
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: "Too many requests, please try again later.",
 });
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 dotenv.config({ path: path.resolve(__dirname, "../../hidden.env") });
-
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../../public")));
-app.use("/fetch-grades", limiter);
+app.use("/startAutomation", limiter);
+app.use("/stopAutomation", limiter);
+let crontask = null;
 
+app.post("/startAutomation", limiter, async function (req, res) {
+  if (!crontask) {
+    try {
+      crontask = cron.schedule("*0 * * * *", async () => {
+        const ditname = process.env.ditusername;
+        const ditpass = process.env.ditpassword;
+        const email = process.env.email;
 
+        const freshID = await getFreshCookie(ditname, ditpass);
 
-app.post("/fetch-grades", async (req, res) => {
-  try {
-    const username = req.body.userName;
-    const password = req.body.userPassword;
-    const email = req.body.userEmail;
-
-    if (!username || !password || !email) {
-      throw new Error("Missing info");
-    }
-    if (!email.includes("@")) throw new Error("Email type");
-    const freshID = await getFreshCookie(`${username}`, `${password}`);
-
-    if (freshID) {
-      await getData(freshID, email);
+        if (freshID) {
+          await getData(freshID, email);
+        }
+      });
       res.status(200).json({
-        type: "Success",
-        message: "No errors occured!",
+        message: "Automation started successfully",
+      });
+    } catch (error) {
+      return res.status(501).json({
+        type: "Failure",
+        message: "Failed to schedule automation.",
       });
     }
-  } catch (error) {
-    if (error.message === "Missing info") {
-      return res.status(400).json({
-        type: "Failure",
-        error: "Missing input",
-        message: "Some input is missing",
-      });
-    } else if (error.message === "Invalid Credentials") {
-      return res.status(500).json({
-        type: "Failure",
-        error: "Invalid DIT account or name",
-        message: "Failed to login on DIT uoi",
-      });
-    } else if (error.message === "Failed to fetch grades") {
-      return res.status(502).json({
-        type: "Failure",
-        error: "Internal Server Error",
-        message: "Failed to fetch grades",
-      });
-    } else if (error.message === "Opening database") {
-      return res.status(500).json({
-        type: "Failure",
-        error: "Database Open error",
-        message: "Couldnt open datbase",
-      });
-    } else if (error.message === "Finding JSESSID") {
-      return res.status(500).json({
-        type: "Failure",
-        error: "JSESSID failed",
-        message: "DIT login success, jsessid capture failed",
-      });
-    } else if (error.message === "Fetching Grades") {
-      return res.status(500).json({
-        type: "Failure",
-        error: "Fetching Grades",
-        message: "Failed to fetch grades from DIT",
-      });
-    } else if (error.message === "DB connection")
-      return res.status(501).json({
-        type: "Failure",
-        error: "Database Connection",
-        message: "Couldnt connect to database",
-      });
-    else if (error.message === "Opening database")
-      return res.status(501).json({
-        type: "Failure",
-        error: "Opening Database",
-        message: "Couldnt open database",
-      });
-    else if (error.message === "Prepare statement")
-      return res.status(501).json({
-        type: "Failure",
-        error: "Prepare Statement",
-        message: "Failed to prepare database insert statement",
-      });
-    else if (error.message === "Finalize statement")
-      return res.status(501).json({
-        type: "Failure",
-        error: "Finalize statement",
-        message: "Failed to finilize database insert statement",
-      });
-    else if (error.message === "Commit DB")
-      return res.status(501).json({
-        type: "Failure",
-        error: "Commit Database",
-        message: "Failed to Commit changed to database",
-      });
-    else if (error.message === "Parsing DB")
-      return res.status(501).json({
-        type: "Failure",
-        error: "Database parsing",
-        message: "Failed to parse data from database",
-      });
-    else if (error.message === "Send Email") {
-      return res.status(501).json({
-        type: "Failure",
-        error: "Send Email",
-        message: "Failed to send email.",
-      });
-    }
-    else if((error.message === "Email type")){
-      return res.status(501).json({
-        type: "Failure",
-        error: "Invalid Email type",
-        message: "Please use @",
-      });
-    } 
-    else {
+  } else {
+    return res.status(400).json({
+      type: "Info",
+      message: "Automation is already active.",
+    });
+  }
+});
+
+app.post("/stopAutomation", async function (req, res) {
+  if (crontask) {
+    try {
+      crontask.stop();
+      crontask = null;
+    } catch (error) {
       return res.status(501).json({
         type: "Failure",
         error: "Unexpected Error",
         message: "Unexpected error occured",
       });
     }
+    res.status(200).json({
+      type: "Success",
+      message: "Successfully stopped automation",
+    });
+  } else {
+    return res.status(501).json({
+      type: "Failure",
+      error: "No active automation",
+      message: "Automation is already inactive",
+    });
   }
 });
-
 app.listen(port);
-
-cron.schedule("*/1 * * * *", async () => {
-  try {    
-    const ditname = process.env.ditusername; 
-    const ditpass = process.env.ditpassword;
-    const email = process.env.email;
-
-    
-    const freshID = await getFreshCookie(ditname, ditpass);
-
-    if (freshID) {
-      await getData(freshID, email);
-      
-    }
-  } catch (error) {
-    console.error("❌ Σφάλμα στο αυτόματο loop:", error.message);
-  }
-});
